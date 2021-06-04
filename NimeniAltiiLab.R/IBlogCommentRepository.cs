b1ee133900d62a3fe1403f90bb389453fbcc2431
systemsmag <systemsmag1992@gmail.com>
2021-06-05 02:42:15 +0300

    Completed repositories.

---
+++ NimeniAltiiLab.R/IBlogCommentRepository.cs
@@ -0,0 +1,20 @@
+﻿using NimeniAltii.Models.BlogComment;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimeniAltiiLab.Repository
+{
+    public interface IBlogCommentRepository
+    {
+        public Task<BlogComment> UpsertAsync(BlogCommentCreate blogCommentCreate, int applicationUserId);
+
+        public Task<List<BlogComment>> GetAllAsync(int blogId);
+
+        public Task<BlogComment> GetAsync(int blogCommentId);
+
+        public Task<int> DeleteAsync(int blogCommentId);
+
+    }
+}
---
+++ NimeniAltiiLab.R/IPhotoRepository.cs
@@ -0,0 +1,21 @@
+﻿using NimeniAltii.Models.Photo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimeniAltiiLab.Repository
+{
+   public interface IPhotoRepository
+    {
+        public Task<Photo> InsertAsync(PhotoCreate photoCreate, int ApplicationUserId);
+
+        public Task<Photo> GetAsync(int photoId);
+
+        public Task<List<Photo>> GetAllByUserIdAsync(int ApplicationUserId);
+
+        public Task<int> DeleteAsync(int photoId);
+
+
+    }
+}
